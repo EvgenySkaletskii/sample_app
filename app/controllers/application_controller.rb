38fd281422f380fd1176e1bf82eccebbeb96812f
Evgeny Skaletskii <evgeny.skaletskii@jetruby.com>
2021-12-21 10:23:40 +0300

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
   def hello
-    render html: 'Hello, world!'
+    render html: "Hello, world!"
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
-require 'minitest/reporters'
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
+require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
@@ -11,4 +11,9 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
   # Add more helper methods to be used by all tests here...
+
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
