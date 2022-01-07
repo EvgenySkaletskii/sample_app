ddccbd281baab82ca7ade614d14a4b4a685b9ec5
Evgeny Skaletskii <evgeny.skaletskii@jetruby.com>
2022-01-07 11:00:51 +0300

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,4 +16,23 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  # Log in as a particular user.
+  def log_in_as(user, password: "password", remember_me: "1")
+    post login_path, params: { session: { email: user.email,
+                                         password: password,
+                                         remember_me: remember_me } }
+  end
 end
